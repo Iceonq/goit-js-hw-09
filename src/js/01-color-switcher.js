438c1603f580f88972a60d36b58ed89806c21e59
Iceonq <bartek.sosnowicz@gmail.com>
2023-01-16 09:19:06 +0100

    1st homework

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector(`[data-stop]`);
+const documentBackground = document.querySelector('body');
+
+stopButton.setAttribute('disabled', '');
+startButton.addEventListener('click', () => {
+  startButton.setAttribute('disabled', '');
+  stopButton.removeAttribute('disabled', '');
+  timer = setInterval(() => {
+    let colorRandomized = getRandomHexColor();
+    documentBackground.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(timer);
+  startButton.removeAttribute('disabled', '');
+  stopButton.setAttribute('disabled', '');
+});
