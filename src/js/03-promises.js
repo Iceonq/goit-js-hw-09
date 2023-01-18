950f3b4a46dd3ae04bc2e2f5deef1c20070c598d
Iceonq <bartek.sosnowicz@gmail.com>
2023-01-18 23:40:27 +0100

    1-3 hw

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,26 @@
+const delayInput = document.querySelector('[name = delay]');
+const stepInput = document.querySelector('[name = step]');
+const amountInput = document.querySelector('[name = amount]');
+const promiseButton = document.querySelector('button');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
     // Reject
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
   }
 }
+
+promiseButton.addEventListener('click', e => {
+  e.preventDefault();
+  setTimeout(() => {
+    for (let i = 1; i <= amountInput.value; i++) {
+      setTimeout(() => {
+        createPromise(i, delayInput.value);
+      }, stepInput.value * i);
+    }
+  }, delayInput.value);
+});
